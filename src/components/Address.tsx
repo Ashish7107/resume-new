7a4f4a428fad2371daf0b1e0c4825c3c5c3be932
Ashish7107 <yadavashish7107@gmail.com>
2023-08-06 18:29:24 +0530

    adding clickable call and email

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -4,14 +4,19 @@ const Address = () => {
   return (
     <div className="container ">
       <div className="address">
-        <i className="fa-solid fa-phone" style={{ paddingRight: "20px" }}></i>
+        <a href="tel:+917387056875">
+          <i className="fa-solid fa-phone" style={{ paddingRight: "20px" }}></i>
+        </a>
         <h6 className="">+91-7387056875</h6>
       </div>
       <div className="address">
-        <i
-          className="fa-solid fa-envelope"
-          style={{ paddingRight: "20px" }}
-        ></i>
+        <a href="mailto:yadavashish7107@gmail.com">
+          <i
+            className="fa-solid fa-envelope"
+            style={{ paddingRight: "20px" }}
+          ></i>
+        </a>
+
         <h6>yadavashish7107@gmail.com</h6>
       </div>
     </div>
